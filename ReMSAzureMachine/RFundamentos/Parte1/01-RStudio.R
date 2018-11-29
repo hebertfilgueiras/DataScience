a196b51ccbf9f9faaa21e26696f50f0cb508e961
hebertfilgueiras <hebertfilgueiras@gmail.com>
2018-11-29 15:49:23 -0200

    criacao de script

---
+++ ReMSAzureMachine/RFundamentos/Parte1/01-RStudio.R
@@ -0,0 +1,32 @@
+#Nome dos contributors
+contributors()
+
+sessionInfo()
+
+print('Ola RRRRR')
+
+plot(1:30)
+
+hist(rnorm(10))
+
+install.packages('randomForest')
+
+install.packages('randomForest')
+
+install.packages('ggplot2')
+
+#carrega pacote na area de trabalho
+library(ggplot2)
+
+# remove pacote da area de trabalho
+detach(package:ggplot2)
+
+help(mean)
+?mean
+
+# se nao souber nome da funcao
+help.search('randomForest')
+help.search('matPLot')
+??matPlot
+RSiteSearch('matPlot')
+example('matplot')
